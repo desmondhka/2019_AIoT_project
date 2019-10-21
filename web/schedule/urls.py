be7890ffc9298751801da508c1ae175284a57c10
desmondhka <56147320+desmondhka@users.noreply.github.com>
2019-10-21 09:32:58 +0800

    fix some issues
    1. reroute the urls
    2. set minDate of datepicker

--- web/schedule/urls.py
+++ web/schedule/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+	path('', views.showtable, name='showtable'),
     path('schedule', views.showtable, name='showtable'),
     path('schedule/<gameDate>', views.showtable, name='showtable'),
 ]
